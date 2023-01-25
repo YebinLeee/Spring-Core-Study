3c1cca485486c2d7d7cb0c52c3a3c38e26c7495b
yebinleee <lyb2325@gmail.com>
2023-01-25 11:29:39 +0900

    refactor: AppConfig 리팩터링(역할과 구현이 명확히 드러나도록 변경)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,18 +1,12 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-<<<<<<< HEAD
-=======
-import hello.core.discount.FixedDiscountPolicy;
-import hello.core.discount.RatedDiscountPolicy;
->>>>>>> 9622a20e94c04855d4687fffcbcd8ad5478992b9
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
+
 
 public class OrderServiceImpl implements OrderService{
 
-<<<<<<< HEAD
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
@@ -20,11 +14,7 @@ public class OrderServiceImpl implements OrderService{
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-=======
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    // private final DiscountPolicy discountPolicy = new FixedDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RatedDiscountPolicy();
->>>>>>> 9622a20e94c04855d4687fffcbcd8ad5478992b9
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
