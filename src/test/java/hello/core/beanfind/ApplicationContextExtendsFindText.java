34d6719094a6b94dbbefa16d4e95fd43f677f2a1
yebinleee <lyb2325@gmail.com>
2023-01-26 11:41:37 +0900

    test: 상속 관계에 따른 빈 조회

---
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindText.java
@@ -0,0 +1,74 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixedDiscountPolicy;
+import hello.core.discount.RatedDiscountPolicy;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class ApplicationContextExtendsFindText {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+    @Test
+    @DisplayName("부모 타입으로 조회 시, 자식이 둘 이상 있으면, 중복 오류가 발생한다.")
+    void findBeanByParentTypeDuplicate(){
+        Assertions.assertThrows(NoUniqueBeanDefinitionException.class,
+                ()-> ac.getBean(DiscountPolicy.class));
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 조회 시, 자식이 둘 이상 있으면, 빈 이름ㅇ르 지정하면 된다.")
+    void findBeanByParentTypeBeanName(){
+        DiscountPolicy ratedDiscountPolicy = ac.getBean("ratedDiscountPolicy", DiscountPolicy.class);
+        org.assertj.core.api.Assertions.assertThat(ratedDiscountPolicy).isInstanceOf(RatedDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("특정 하위 타입으로 조회")
+    void findBeanBySubType(){
+        RatedDiscountPolicy bean = ac.getBean(RatedDiscountPolicy.class);
+        org.assertj.core.api.Assertions.assertThat(bean).isInstanceOf(RatedDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기")
+    void findAllBeanByParentType(){
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+        org.assertj.core.api.Assertions.assertThat(beansOfType.size()).isEqualTo(2);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기 - Object")
+    void findAllBeansByObjectType(){
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+    }
+    @Configuration
+    static class TestConfig {
+        @Bean
+        public DiscountPolicy ratedDiscountPolicy(){
+            return new RatedDiscountPolicy();
+        }
+
+        @Bean
+        public DiscountPolicy fixedDiscountPolicy(){
+            return new FixedDiscountPolicy();
+        }
+    }
+
+}
